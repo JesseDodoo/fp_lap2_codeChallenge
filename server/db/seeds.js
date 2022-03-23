3fd00efbfc14829c36014c8123702ea32da3584c
jesse dodoo <jesse.dodoo@gmail.com>
2022-03-23 15:17:02 +0000

    matched the DB_NAME with MONGO_INITDB_DATABASE

--- server/db/seeds.js
+++ server/db/seeds.js
@@ -1,5 +1,10 @@
-const db = connect("mongodb://localhost:4000/codechallenge")
+const db = connect("mongodb://localhost:27017/codechallenge")
 
 db.users.drop()
 
+db.users.insertOne([
+    {title: 'All about me', 
+    author: 'jesse', 
+    message: 'This is my message'
+}])
 
