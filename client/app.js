e34c0bc757b8adb9c5bb9a3725cb72442d558b3c
jesse dodoo <jesse.dodoo@gmail.com>
2022-03-23 16:56:25 +0000

    minimalist styling on frontend

--- client/app.js
+++ client/app.js
@@ -1,6 +1,6 @@
 const form = document.querySelector('#new-message-form');
 
-// form.addEventListener('submit', submitAnonMessage);
+form.addEventListener('submit', submitAnonMessage);
 
 getAllMessages();
 
@@ -9,4 +9,26 @@ async function getAllMessages(){
     const json =  await response.json()
     console.log(json)
     return json;
+}
+
+
+function submitAnonMessage(e){
+    e.preventDefault();
+
+    const messageData = {
+        title: e.target.title.value,
+        author: e.target.author.value,
+        message: e.target.message.value
+    }
+
+    const options =  {
+        method: 'POST', 
+        body: JSON.stringify(messageData),
+        headers: { "Content-Type": "application/json"}
+    };
+
+    fetch('http://localhost:4000/users', options)
+        .then(r => r.json())
+        .then(() => e.target.reset())
+        .catch(console.log(err))
 }
