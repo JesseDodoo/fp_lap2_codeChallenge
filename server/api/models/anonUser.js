c8bf549a2f2254a782e83520e1714474945218ce
jesse dodoo <jesse.dodoo@gmail.com>
2022-03-23 13:09:54 +0000

    used dummy data to test route paths and to retrieve data from database

--- server/api/models/anonUser.js
+++ server/api/models/anonUser.js
@@ -1,2 +1,27 @@
 const { init } = require('../dbConfig')
 
+class User {
+    constructor(data){
+        this.id = data.id,
+        this.title = data.title,
+        this.author = data.author,
+        this.message =  data.message
+    }
+
+    static get all() {
+        return new Promise (async (resolve, reject) => {
+            try {
+                const db = await init()
+                const userData = await db.collection('anonUser').find().toArray()
+                const anonUser = userData.map(a => new User({ ...a, id: a._id }))
+                resolve(anonUser);
+            } catch (err) {
+                console.log(err);
+                reject("Error retrieving User")
+            }
+        })
+    }
+
+    
+
+}
